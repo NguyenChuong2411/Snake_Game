503ccc64d8fc6227fe749cd03a25de9298778d7d
NguyenChuong2411 <165977564+NguyenChuong2411@users.noreply.github.com>
2024-10-27 22:44:27 +0700

    Update Assets.cs
    Edit Asset references

--- Assets/Custom/Assets.cs
+++ Assets/Custom/Assets.cs
@@ -1,1 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Custom {
+
+    /*
+     * Global Asset references
+     * Edit Asset references in the prefab Custom/Resources/CustomAssets
+     * */
+    public class Assets : MonoBehaviour {
+
+        // Internal instance reference
+        private static Assets _i; 
+
+        // Instance reference
+        public static Assets i {
+            get { 
+                if (_i == null) _i = (Instantiate(Resources.Load("CustomAssets")) as GameObject).GetComponent<Assets>(); 
+                return _i; 
+            }
+        }
+
+
+        // All references
+
+        public Sprite s_White;
+
+    }
+
+}
 
